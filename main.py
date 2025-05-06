5813004de9562ed04bdfb29cfce907de43511dde
abdul <theasamad6@gmail.com>
2025-05-06 19:10:56 +0500

    Added models

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI, HTTPException, Depends
-from models import GenreUrl, Band, BandCreate, Album
+from fastapi import UploadFile, File, Form
+from models import (GenreUrl,
+                    Band,
+                    BandCreate,
+                    Album,
+                    Task,
+                    Profile,
+                    ProfileRegister,
+                    Feedback)
 from sqlmodel import Session
+# from uuid import UUID
+from datetime import datetime, timezone
+from pydantic import EmailStr
+
+from typing import Optional
 from contextlib import asynccontextmanager
 from db import init_db, get_session
 from sqlmodel import select
@@ -59,6 +72,124 @@ async def get_bands(genre: GenreUrl | None = None,
         band_list = [b for b in band_list if len(b.albums) > 0]
         # print(bands)
     return band_list
+
+
+@app.post("/tasks")
+async def get_task(
+    task: Task,
+    session: Session = Depends(get_session)
+):
+    task = Task(title=task.title, task=task.task, completed=task.completed)
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+
+@app.get("/tasks")
+async def get_tasks(
+    completed: Optional[bool] = None,
+    session: Session = Depends(get_session)
+):
+    if completed is True:
+        return session.exec(select(Task).where(Task.completed.is_(True))).all()
+    elif completed is False:
+        return session.exec(select(Task).where(
+            Task.completed.is_(False))).all()
+    else:
+        return session.exec(select(Task)).all()
+
+
+@app.get("/tasks/{task_id}")
+async def get_tasks_id(
+    task_id: int,
+    session: Session = Depends(get_session)
+):
+    task = session.get(Task, task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.post("/profiles")
+async def create_profile(
+    profile_data: ProfileRegister,
+    session: Session = Depends(get_session)
+):
+    profile = Profile(name=profile_data.name,
+                      email=profile_data.email,
+                      password=profile_data.password,
+                      )
+
+    session.add(profile)
+    session.commit()
+    session.refresh(profile)
+    return profile
+
+
+@app.get("/profiles")
+async def get_profiles(
+    session: Session = Depends(get_session)
+):
+    profiles = session.exec(select(Profile)).all()
+    return profiles
+
+
+@app.get("/profiles/{profile_id}")
+async def get_profile(
+    profile_id: int,
+    session: Session = Depends(get_session)
+):
+    profile = session.get(Profile, profile_id)
+    if profile is None:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return profile
+
+
+@app.post("/feedback")
+async def create_feedback(
+    email: EmailStr = Form(...),
+    message: str = Form(...),
+    file: UploadFile = File(...),
+    user_id: int = Form(...),
+    session: Session = Depends(get_session)
+):
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    feedback = Feedback(
+        email=email,
+        message=message,
+        user_id=user_id,
+        file_path = file_path,
+        timestamp=datetime.now(timezone.utc)
+    )
+
+    session.add(feedback)
+    session.commit()
+    session.refresh(feedback)
+    return feedback
+
+
+@app.get("/feedback")
+async def get_feedback(
+    session: Session = Depends(get_session)
+):
+    feedback = session.exec(select(Feedback)).all()
+    return feedback
+
+
+@app.get("/feedback/{feedback_id}")
+async def get_feedback_id(
+    feedback_id: int,
+    session: Session = Depends(get_session)
+):
+    feedback = session.get(Feedback, feedback_id)
+    if feedback is None:
+        raise HTTPException(status_code=404, detail="Feedback not found")
+    return feedback
 
 # _-------------------------------------------------------------------------_#
 # Below is the code without SQLModel
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from datetime import date
+from datetime import date, datetime, timezone
 from enum import Enum
-from pydantic import field_validator
+from pydantic import field_validator, EmailStr, model_validator
 from sqlmodel import SQLModel, Field, Relationship
 
 
@@ -48,46 +48,65 @@ class Band(BandBase, table=True):
     id: int = Field(default=None, primary_key=True)
     albums: list[Album] = Relationship(back_populates="band")
 
-# _-------------------------------------------------------------------------_#
-# Below is the code without SQLModel
-# _-------------------------------------------------------------------------_#
-# from datetime import date
-# from enum import Enum
-# from pydantic import BaseModel, field_validator
-# from sqlmodel import SQLModel, Field
 
-# class GenreUrl(str, Enum):
-#     ROCK = "rock"
-#     METAL = "metal"
-#     GRUNGE = "grunge"
-#     ELECTRONIC = "electronic"
-#     SHOEGAZE = "shoegaze"
+class TaskBase(SQLModel):
+    title: str
+    task: str
+    description: str | None = None
+    due_date: date | None = None
 
 
-# class GenreChoices(str, Enum):
-#     ROCK = "Rock"
-#     METAL = "Metal"
-#     GRUNGE = "Grunge"
-#     ELECTRONIC = "Electronic"
-#     SHOEGAZE = "Shoegaze"
+class Task(TaskBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    completed: bool = Field(default=False)
+
+    @field_validator("task", mode="before")
+    def title_case(cls, v):
+        return v.title()
 
 
-# class Album(BaseModel):
-#     title: str
-#     release_date: date
+class ProfileBase(SQLModel):
+    name: str
+    email: EmailStr
+    password: str
 
 
-# class BandBase(BaseModel):
-#     name: str
-#     genre: GenreChoices
-#     albums: list[Album] = []
+class Profile(ProfileBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    feedbacks: list["Feedback"] = Relationship(back_populates="profile")
+    # tasks: list[Task] = Relationship(back_populates="profile")
+    # bands: list[Band] = Relationship(back_populates="profile")
 
 
-# class BandCreate(BandBase):
-#     @field_validator("genre", mode="before")
-#     def title_case(cls, v):
-#         return v.title()  # rock -> Rock
+class ProfileRegister(ProfileBase):
+    confirm_password: str
+
+    @model_validator(mode="before")
+    def password_match(cls, values):
+        password = values.get("password")
+        confirm_password = values.get("confirm_password")
+        if password != confirm_password:
+            raise ValueError("Passwords do not match")
+        return values
+
+    @field_validator("email", mode="before")
+    def email_lower(cls, v):
+        return v.lower()  # rock -> Rock
 
 
-# class BandWithID(BandBase):
-#     id: int
+class FeedbackBase(SQLModel):
+
+    email: EmailStr
+    message: str
+    user_id: int = Field(default=None, foreign_key="profile.id")
+    file_path: str | None = None
+# default_factory calls the function at run time while default will uses a
+# fixed value
+    timestamp: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+        )
+
+
+class Feedback(FeedbackBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    profile: Profile = Relationship(back_populates="feedbacks")
